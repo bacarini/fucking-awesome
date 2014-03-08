192745451d09d89dcb0b24ac73c4c65c70935200
Bruno Bacarini <bacarini.bruno@gmail.com>
2014-03-08 19:20:22 -0300

    change the color of the links

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
   <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
   <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
   <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
-
+  <link rel="shortcut icon" href="http://www.hashtagcool.com.br/wp-content/themes/fucking-awesome/favicon.ico" type="image/x-icon">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
 
